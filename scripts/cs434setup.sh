8ee1722d6a32c622f41a70ac548c4c01c0550e03
Jazel Canseco <jazel.canseco@gmail.com>
2017-12-09 01:49:55 +0900

    Introduced variable in CS434 set-up script to show that C-m means carriage return in tmux.

--- scripts/cs434setup.sh
+++ scripts/cs434setup.sh
@@ -3,26 +3,30 @@
 PATH_TO_PROJECT="/home/jcanseco/Workspace/CS434/project/"
 PATH_TO_SANDBOX="/home/jcanseco/Workspace/CS434-Sandbox/"
 
+CR=C-m # Carriage return in Tmux
+
+tmux rename-session "CS434"
+
 # Set up code window
 tmux rename-window "code"
-tmux send-keys "cd $PATH_TO_PROJECT; clear" C-m
-tmux send-keys "vim build.sbt src/**/*.scala" C-m
+tmux send-keys "cd $PATH_TO_PROJECT; clear" $CR
+tmux send-keys "vim build.sbt src/**/*.scala" $CR
 
 tmux split-window -v
-tmux send-keys "cd $PATH_TO_PROJECT; clear" C-m
+tmux send-keys "cd $PATH_TO_PROJECT; clear" $CR
 tmux resize-pane -D 15
-tmux send-keys "sbt" C-m
+tmux send-keys "sbt" $CR
 
 # Set up sandbox window
 tmux new-window -n "sandbox"
-tmux send-keys "cd $PATH_TO_SANDBOX; clear" C-m
-tmux send-keys "ls" C-m
-tmux send-keys "ls outputs" C-m
+tmux send-keys "cd $PATH_TO_SANDBOX; clear" $CR
+tmux send-keys "ls" $CR
+tmux send-keys "ls outputs" $CR
 
 # Set up git window
 tmux new-window -n "git"
-tmux send-keys "cd $PATH_TO_PROJECT; clear" C-m
-tmux send-keys "git status" C-m
+tmux send-keys "cd $PATH_TO_PROJECT; clear" $CR
+tmux send-keys "git status" $CR
 
-# Switch back to code window
+# Switch to code window
 tmux select-window -t "code"
