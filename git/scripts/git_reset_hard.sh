ed430fafe390c3e818a149dbd55d1add8901554d
Jazel Canseco <jazel.canseco@gmail.com>
2018-07-19 02:10:07 -0400

    Set scripts to accept user input right away without waiting for enter key.

--- git/scripts/git_reset_hard.sh
+++ git/scripts/git_reset_hard.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Helper script for resetting the git working tree to HEAD.
 
-read -p "git: Resetting the working tree to HEAD. Continue? [Y/n]: " opt
+read -n1 -p "git: Resetting the working tree to HEAD. Continue? [Y/n]: " opt
+printf "\n"
 if [[ $opt == "Y" ]]; then
     git add -A && git reset --hard
 fi
