0081b4a1c33db4c7edce3185362d7f99bc670daa
Jazel Canseco <jazel.canseco@gmail.com>
2018-07-19 01:48:30 -0400

    Modified git_reset_hard.sh to be more consistent with v.sh.

--- git/scripts/git_reset_hard.sh
+++ git/scripts/git_reset_hard.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+# Helper script for resetting the git working tree to HEAD.
 
 read -p "git: Resetting the working tree to HEAD. Continue? [Y/n]: " opt
-
-if [ $opt == "Y" ]; then
-  git add -A && git reset --hard
+if [[ $opt == "Y" ]]; then
+    git add -A && git reset --hard
 fi
